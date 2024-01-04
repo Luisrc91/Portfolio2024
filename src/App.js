023a54e91f18430c042dfb75818e8a4795d743ab
Luis <luisrc91@live.com>
2024-01-04 15:53:31 -0500

    added imports pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 // import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
+import About from './Pages/About';
+import ContactMe from './Pages/ContactMe';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
+
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <header className="App-header">
+      <NavBar />  
       
       </header>
+        
+
+
+
+      </Router>
     </div>
   );
 }

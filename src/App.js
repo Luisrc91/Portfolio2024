892c3c87d4867e3e2d15a3c73e676055a6403047
Luis <luisrc91@live.com>
2024-01-15 09:50:51 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 // import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-import About from './Pages/About';
-import ContactMe from './Pages/ContactMe';
-import Home from './Pages/Home';
-import Projects from './Pages/Projects';
-
-
+import About from "./Pages/About";
+import ContactMe from "./Pages/ContactMe";
+import Home from "./Pages/Home";
+import Projects from "./Pages/Projects";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <header className="App-header">
-      <NavBar />  
-      
-      </header>
-        
+        <header className="App-header">
+          <NavBar />
+        </header>
 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AboutMe" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/ContactMe" element={<ContactMe />} />
+        </Routes>
 
-
+        <Footer />
       </Router>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
+import './NavBar.css'
 
-export default function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="home" smooth={true} duration={500}>Home</Link>
+            <Link to="about" smooth={true} duration={500}>About</Link>
+            <Link to="projects" smooth={true} duration={500}>Projects</Link>
+            <Link to="contact" smooth={true} duration={500}>Contact</Link>
+        </nav>
+    );
 }
+
+export default Navbar;

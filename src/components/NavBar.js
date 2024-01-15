d769c441d7fd953c9e250313866d3fe4994a9fd3
Luis <luisrc91@live.com>
2024-01-15 10:28:28 -0500

    fix navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
-import './NavBar.css'
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <Link to="home" smooth={true} duration={500}>Home</Link>
-            <Link to="about" smooth={true} duration={500}>About</Link>
-            <Link to="projects" smooth={true} duration={500}>Projects</Link>
-            <Link to="contact" smooth={true} duration={500}>Contact</Link>
+            <Link to="/">Home</Link>
+            <Link to="/About">About</Link>
+            <Link to="/Projects">Projects</Link>
+            <Link to="/Contact">Contact</Link>
         </nav>
     );
 }

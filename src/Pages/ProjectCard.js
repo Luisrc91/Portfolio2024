3c1a6f7144f0f0895ccb5586155b3295114be4f1
Luis <luisrc91@live.com>
2024-01-24 14:39:20 -0500

    added card

--- src/Pages/ProjectCard.js
+++ src/Pages/ProjectCard.js
@@ -1,20 +1,36 @@
-// ProjectCard.js
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
 const ProjectCard = ({ projectName, imageUrl, projectUrl }) => {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={imageUrl} alt={`${projectName} Preview`} />
-      <Card.Body>
-        <Card.Title>{projectName}</Card.Title>
-        <Button variant="primary" href={projectUrl} target="_blank" rel="noopener noreferrer">
+    <div style={cardStyle}>
+      <img src={imageUrl} alt={`${projectName} Preview`} style={imageStyle} />
+      <div style={contentStyle}>
+        <h2>{projectName}</h2>
+        <a href={projectUrl} target="_blank" rel="noopener noreferrer">
           View Project
-        </Button>
-      </Card.Body>
-    </Card>
+        </a>
+      </div>
+    </div>
   );
 };
 
+const cardStyle = {
+  border: '1px solid #ddd',
+  borderRadius: '8px',
+  overflow: 'hidden',
+  width: '600px', 
+  margin: '16px',
+ 
+};
+
+const imageStyle = {
+  width: '100%',
+  height: 'auto',
+};
+
+const contentStyle = {
+  padding: '16px',
+};
+
 export default ProjectCard;
+
--- src/Pages/ProjectsPic.js
+++ src/Pages/ProjectsPic.js
@@ -1,29 +1,42 @@
+// Projects.js
 import React from 'react';
-import './Projects.css'; // Import the CSS file
+import ProjectCard from './ProjectCard';
+import './Projects.css'
+import Recipe from '../assets/ABCL-recipe-book2023.png'
+import Trivia from '../assets/triviagameJs.png'
+import Event from '../assets/eventplanner-9d21c.web.app_login.png'
+import RestRant from '../assets/localhost_3000_.png'
+const projects = [
+  { id: 1, name: 'Event Planner', imageUrl: Event, url: 'https://github.com/Luisrc91/eventPlanner_frontend' },
+  { id: 2, name: 'RestRant', imageUrl: RestRant, url: 'https://github.com/Luisrc91/rest-rant-monorepo' },
+  { id: 3, name: 'ABCL-Recipe Book', imageUrl:Recipe, url: 'https://github.com/BLDubroff/ABCL-Recipe-Book?tab=readme-ov-file' },
+  { id: 4, name: 'Trivia Game', imageUrl: Trivia, url: 'https://github.com/Luisrc91/TriviaGameP1BG' },
+];
 
-const ProjectsPic = () => {
-    return (
-        <section className="projects-section">
-            <div className="projects-content">
-                <h2>Projects</h2>
-                <p>
-                    Explore some of my featured projects below:
-                </p>
-                <div className="project-card">
-                    <h3>Project 1</h3>
-                    <p>Description of Project 1.</p>
-                </div>
-                <div className="project-card">
-                    <h3>Project 2</h3>
-                    <p>Description of Project 2.</p>
-                </div>
-             
-            </div>
-            <div>
-            <a style={{fontSize:'20px', textDecoration:'none', color: 'black'}} href="/Projects"><h3>Click Github Projects here!</h3></a>
-            </div>
-        </section>
-    );
-}
+const Projects = () => {
+  return (
+    <div className="projects-section">
+      <h1>My Projects</h1>
+      <div className="projects-content">
+        <div className="project-cards-container">
+          {projects.map(project => (
+            <ProjectCard
+              key={project.id}
+              projectName={project.name}
+              imageUrl={project.imageUrl}
+              projectUrl={project.url}
+            />
+          ))}
+        </div>
+      </div>
+      <div>
+        <a className="gitHubLink" href="/Projects">
+          My GitHub Projects
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectsPic;
+export default Projects;
+

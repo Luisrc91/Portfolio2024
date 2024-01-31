9da477ed38dfcca91e14dc6da2d53b1e2054c1c6
Luis <luisrc91@live.com>
2024-01-31 08:50:35 -0500

    added about me

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,10 +7,11 @@ const About = () => {
             <div className="about-content">
                 <h2>About Me</h2>
                 <p>
-                    Welcome to my portfolio! I am a passionate software developer with expertise in JavaScript, Python, React, TypeScript, ExpressJS, NodeJs, SQL and MongoDB.
+                    Welcome to my portfolio! <br /> I am a passionate software developer with expertise in JavaScript, Python, React, TypeScript, ExpressJS, NodeJs, SQL and MongoDB.
                 </p>
                 <p>
-                    Hi, Im Luis <br /> Im from North Carolina, born in Mexico City  
+                    Hi, Im Luis <br /> Im from North Carolina, I have always passionate about technology from TVs, Cell-phones and video games!! <br />
+                    I enjoy building websites from Front-end devolopment to Bank-end devolopment; I graduated from a North Carolina State University of a Software Developer Professional Certification. 
                 </p>
             </div>
         </section>

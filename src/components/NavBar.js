e96a3ad4189f76bff2905a64750f16f5f38cd5dc
Luis <luisrc91@live.com>
2024-02-05 08:08:42 -0500

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css';
+import './NavBar.css'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);

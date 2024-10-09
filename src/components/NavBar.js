3a63ba0c51e5ee247d24176926a4cc5b4e706dae
Luis <luisrc91@live.com>
2024-10-09 11:22:29 -0400

    added tailwind

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,52 +1,28 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css'
+import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+import "./NavBar.css";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav)
+  }
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
-  const closeMenu = () => {
-    setIsOpen(false);
-  };
 
   return (
-    <nav className="navbar">
-      <button className="hamburger" onClick={toggleMenu}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </button>
-      <span className={`name ${isOpen ? 'visible' : ''}`}>Luis Ramirez</span>
-      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
-        <li>
-          <Link to="/" onClick={closeMenu}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/About" onClick={closeMenu}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/MainProjects" onClick={closeMenu}>
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link to="/ContactMe" onClick={closeMenu}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      
+      <div className="bg-black h-[35px] text-gray-400">
+        <div onClick={handleNav} className="block md:hidden">
+          {nav ? <AiOutlineClose size={35}/> : <AiOutlineMenu size={35}/>}
+
+        </div>
+      </div>
+       
+    </div>
   );
 };
 
 export default Navbar;
-
-

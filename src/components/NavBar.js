9e4b72123d5fa4664422e4c1746b27b3eea548f4
Luis <luisrc91@live.com>
2024-10-09 12:41:21 -0400

    added style to nav bar and effect

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -7,6 +8,10 @@ const Navbar = () => {
 
   const handleNav = () => {
     setNav(!nav);
+  };
+
+  const handleLinkClick = () => {
+    setNav(false);
   };
 
   useEffect(() => {
@@ -34,16 +39,21 @@ const Navbar = () => {
   return (
     <div className="">
       <div className="bg-black h-[90px] text-gray-400 max-auto flex justify-between items-center">
-        <h1 className="text-3xl font-bold bg-gradient-to-r from-red-500 to-white bg-clip-text text-transparent ml-4">Luis Ramirez</h1>
+        <h1 className="text-3xl font-bold bg-gradient-to-r from-red-500 to-white bg-clip-text text-transparent ml-4">
+          Luis Ramirez
+        </h1>
         <ul className="hidden md:flex">
-          <li className="p-5">
-            <a href="#">About</a>
+          <li className="p-5 transition duration-300 hover:text-red-500">
+            <Link to="/">Home</Link>
           </li>
-          <li className="p-5">
-            <a href="#">Projects</a>
+          <li className="p-5 transition duration-300 hover:text-red-500">
+            <Link to="/About">About</Link>
           </li>
-          <li className="p-5">
-            <a href="#">Contact</a>
+          <li className="p-5 transition duration-300 hover:text-red-500">
+            <Link to="/MainProjects">Projects</Link>
+          </li>
+          <li className="p-5 transition duration-300 hover:text-red-500">
+            <Link to="/ContactMe">Contact</Link>
           </li>
         </ul>
         <div onClick={handleNav} className="block md:hidden mr-6">
@@ -53,20 +63,31 @@ const Navbar = () => {
           ref={navRef}
           className={
             nav
-              ? "rounded fixed h-full right-0 top-20 w-[60%] bg-[#0c0c0c] ease-in-out duration-400"
-              : "fixed left-[-100%]"
+              ? "rounded fixed h-full right-0 top-20 w-[40%] bg-[#0c0c0c] ease-in-out duration-400"
+              : "fixed right-[-100%]"
           }
         >
-          <h1 className="text-3lx primary-color m-4">Menu</h1>
+          <h1 className="text-3xl primary-color m-4">Menu</h1>
           <ul className="p-8 text-2xl">
-            <li className="p-2">
-              <a href="#">About</a>
+            <li className="p-2 transition duration-300 hover:text-red-500">
+              <Link to="/" onClick={handleLinkClick}>
+                Home
+              </Link>
             </li>
-            <li className="p-2">
-              <a href="#">Projects</a>
+            <li className="p-2 transition duration-300 hover:text-red-500">
+              <Link to="/About" onClick={handleLinkClick}>
+                About
+              </Link>
             </li>
-            <li className="p-2">
-              <a href="#">Contact</a>
+            <li className="p-2 transition duration-300 hover:text-red-500">
+              <Link to="/MainProjects" onClick={handleLinkClick}>
+                Projects
+              </Link>
+            </li>
+            <li className="p-2 transition duration-300 hover:text-red-500">
+              <Link to="/ContactMe" onClick={handleLinkClick}>
+                Contact
+              </Link>
             </li>
           </ul>
         </div>

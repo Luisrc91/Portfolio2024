847500e6379a2bb74c7fb100d6aaefe982c74584
Luis <luisrc91@live.com>
2024-10-10 15:46:40 -0400

    change color on projects card

--- src/Pages/ProjectCard.js
+++ src/Pages/ProjectCard.js
@@ -2,19 +2,19 @@ import React from "react";
 
 const ProjectCard = ({ projectName, imageUrl, projectUrl }) => {
   return (
-    <div className="border border-gray-300 rounded-lg overflow-hidden w-full max-w-sm mx-auto shadow-lg my-4">
+    <div className="border-transparent bg-gradient-to-l to-gray-900 to-black  rounded-sm overflow-hidden w-full max-w-sm mx-auto shadow-lg my-4">
       <img
         src={imageUrl}
         alt={`${projectName} Preview`}
         className="w-full h-auto"
       />
       <div className="p-4">
-        <h2 className="text-xl font-semibold text-gray-800">{projectName}</h2>
+        <h2 className="text-xl font-semibold text-red-300">{projectName}</h2>
         <a
           href={projectUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-500 hover:text-blue-600 transition-colors"
+          className="text-white hover:text-orange-600 transition-colors"
         >
           View Project
         </a>
